cb9502be1a05b5c88a5368e0c9a9c919a34fe8cd
Narayana <lakkireddy.narayana-reddy@airbus.com>
2024-09-23 21:03:15 +0530

    aligned with items

--- portfolio-webpp/src/components/LandingPageView.tsx
+++ portfolio-webpp/src/components/LandingPageView.tsx
@@ -3,12 +3,16 @@ import "./LandingPageView.scss";
 export const LandingPageView = () => {
   return (
     <div className="container">
-      <header>HEADER</header>
-      <main>
-        <section>SECTION-1</section>
-        <section>SECTION-2</section>
+      <header className="header">HEADER</header>
+      <main className="main main__content">
+        <section className="section">PERSONALDETAILS</section>
+        <section className="section">EXPERIENCES</section>
+        <section className="section">SKILLS</section>
+        <section className="section">PROJECTS</section>
+        <section className="section">EDUCATIONS</section>
+        <section className="section">CONTACT ME</section>
       </main>
-      <footer>FOOTER</footer>
+      <footer className="footer">FOOTER</footer>
     </div>
   );
 };
