44bec1755329035060701d8ab42c09b8cfe27625
Narayana <lakkireddy.narayana-reddy@airbus.com>
2024-09-25 18:30:51 +0530

    card created partially

--- portfolio-webpp/src/components/helper/EducationCard.tsx
+++ portfolio-webpp/src/components/helper/EducationCard.tsx
@@ -1,5 +1,27 @@
 import "./EducationCard.scss";
 
 export const EducationCard = () => {
-  return <div className="card education__card">Education Card</div>;
+  return (
+    <div className="education__card">
+      <div className="card">
+        <div className="card__header">
+          <div className="dots">
+            <span />
+            <span />
+            <span />
+          </div>
+          <h2 className="card__title">Heading Here</h2>
+        </div>
+        <div className="card__content">
+          <div className="card__cont">
+            <div className="description">
+              <h3>DESCRIPTION: </h3>{" "}
+            </div>
+            <div className="stack">STACK</div>
+            <div className="responsibilities">RESPONSIBILITIES</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
