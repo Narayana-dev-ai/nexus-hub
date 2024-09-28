ec90cb748b6341fdc66aedf61dd822b3004f7181
Narayana <lakkireddy.narayana-reddy@airbus.com>
2024-09-28 16:55:12 +0530

    glowing card integrated

--- portfolio-webpp/src/components/cards/GlowCard.tsx
+++ portfolio-webpp/src/components/cards/GlowCard.tsx
@@ -1,12 +1,32 @@
-import "./GlowCard.scss"
+import "./GlowCard.scss";
 
-export const GlowCard = (props: any) => {
+export const GlowCard: React.FC<{
+  company: string;
+  role: string;
+  duration: string;
+  resp: string;
+  stack: string;
+}> = ({ company, role, duration, resp, stack }) => {
   return (
-    <div className="card" role="button">
+    <div className="g_card" role="button">
       <span className="glow"></span>
       <div className="inner">
-        <span>cool</span>Glowing shadows <br />
-        {props.txt}
+        <div className="gl__content">
+          <h2>{company}</h2>
+          <br />
+
+          <div className="gl__role">
+            <h4>{role}</h4>
+            <h4 className="duration">{duration}</h4>
+          </div>
+          <div className="gl__respo">
+            <span className="gl__side">Responsibilities</span>
+            <p>{resp}</p>
+          </div>
+          <div className="gl__stack">
+            <span className="gl__side">Tech Stack:</span> <p>{stack}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- portfolio-webpp/src/components/helper/ExperienceCard.tsx
+++ portfolio-webpp/src/components/helper/ExperienceCard.tsx
@@ -1,3 +1,4 @@
+import { GlowCard } from "../cards/GlowCard";
 import { LineCard } from "../cards/LineCard";
 import "./ExperienceCard.scss";
 
@@ -5,6 +6,29 @@ export const ExperienceCard = () => {
   return (
     <div className="experience__card">
       <LineCard title="Work Experience" />
+      <div className="experience__card__items">
+        <GlowCard
+          company="AIRBUS GROUP INDIA Pvt Ltd"
+          role="Software Engineer 😎"
+          duration="Mar 2024 to till Date"
+          resp="I'm working on both front-end and back-end development processes. I design, develop, and maintain fully-fledged and functioning platforms with databases or servers."
+          stack="React Js, Java, TypeScript, JavaScript, AWS, Docker"
+        />
+        <GlowCard
+          company="AIRBUS GROUP INDIA Pvt Ltd"
+          role="Associate Software Engineer 🙂"
+          duration="Aug 2021 to Mar 2024"
+          resp="I'm working on both front-end and back-end development processes. I design, develop, and maintain fully-fledged and functioning platforms with databases or servers."
+          stack="React Js, Java, TypeScript, JavaScript, AWS, PySpark, Python"
+        />
+        <GlowCard
+          company="Glenwood LLc"
+          role="System Engineer 🙂"
+          duration="May 2021 to Jul 2021"
+          resp="I'm working on front-end. Building React.js UI components and providing best UX with Quality"
+          stack="React Js, Python"
+        />
+      </div>
     </div>
   );
 };
